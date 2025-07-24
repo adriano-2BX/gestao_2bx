659edc0f2be1d61e476f0a43ce9ece8c9512bf1e
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 15:30:11 -0300

    Update api_router.py

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -1,11 +1,11 @@
 # app/api/api_router.py
 from fastapi import APIRouter
-from .endpoints import clientes, projetos # 1. Importe 'projetos'
+from .endpoints import clientes, projetos, tarefas # 1. Importe 'tarefas'
 
 api_router = APIRouter()
 
-# Rota de Clientes (já existia)
 api_router.include_router(clientes.router, prefix="/clientes", tags=["Clientes"])
-
-# 2. Adicione a nova rota de Projetos
 api_router.include_router(projetos.router, prefix="/projetos", tags=["Projetos"])
+
+# 2. Adicione a nova rota de Tarefas
+api_router.include_router(tarefas.router, prefix="/tarefas", tags=["Tarefas"])
