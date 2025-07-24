4b4610439001be531bad363a039eca2c88b203cc
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 15:38:04 -0300

    Update api_router.py

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -1,12 +1,14 @@
 # app/api/api_router.py
 from fastapi import APIRouter
-from .endpoints import clientes, projetos, tarefas, usuarios # 1. Importe 'usuarios'
+from .endpoints import clientes, projetos, tarefas, usuarios, login # 1. Importe 'login'
 
 api_router = APIRouter()
 
+# Rota de autenticação
+api_router.include_router(login.router, tags=["Login"])
+
+# Rotas de recursos
 api_router.include_router(clientes.router, prefix="/clientes", tags=["Clientes"])
 api_router.include_router(projetos.router, prefix="/projetos", tags=["Projetos"])
 api_router.include_router(tarefas.router, prefix="/tarefas", tags=["Tarefas"])
-
-# 2. Adicione a nova rota de Usuários
 api_router.include_router(usuarios.router, prefix="/usuarios", tags=["Usuários"])
