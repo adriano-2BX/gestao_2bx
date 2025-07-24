184043660205add7f4beb890c1415a93d97b040d
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 14:35:33 -0300

    Update clientes.py

--- app/api/endpoints/clientes.py
+++ app/api/endpoints/clientes.py
@@ -1,3 +1,4 @@
+# app/api/endpoints/clientes.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
@@ -9,6 +10,9 @@ router = APIRouter()
 
 @router.post("/", response_model=schemas.Cliente, status_code=201, summary="Cria um novo cliente")
 def create_cliente(cliente: schemas.ClienteCreate, db: Session = Depends(get_db)):
+    """
+    Cria um novo cliente no banco de dados.
+    """
     db_cliente = models.Cliente(**cliente.model_dump())
     db.add(db_cliente)
     db.commit()
@@ -17,5 +21,8 @@ def create_cliente(cliente: schemas.ClienteCreate, db: Session = Depends(get_db)
 
 @router.get("/", response_model=List[schemas.Cliente], summary="Lista todos os clientes")
 def read_clientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """
+    Retorna uma lista de clientes com paginação.
+    """
     clientes = db.query(models.Cliente).offset(skip).limit(limit).all()
     return clientes
