ad76fdb9275bba1d9cf904e35c5a76a40d6e6355
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 17:34:12 -0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -8,3 +8,4 @@ from .aplicacao import Aplicacao
 from .dominio import Dominio
 from .certificado_ssl import CertificadoSSL # Adicione esta linha
 from .conexao import Conexao # Adicione esta linha
+from .time import Time # Adicione esta linha
