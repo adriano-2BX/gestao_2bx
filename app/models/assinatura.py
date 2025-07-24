136fe9312ef799b3d25e5071c2631b0f7c634d73
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 18:19:46 -0300

    Update assinatura.py

--- app/models/assinatura.py
+++ app/models/assinatura.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, ENUM, DATE, DECIMAL, ForeignKey
+# app/models/assinatura.py
+from sqlalchemy import Column, Integer, String, Enum, DATE, DECIMAL, ForeignKey
 from ..core.db import Base
 class Assinatura(Base):
     __tablename__ = "assinaturas"
@@ -6,7 +7,9 @@ class Assinatura(Base):
     cliente_id = Column(Integer, ForeignKey("clientes.id"), nullable=False)
     servico_id = Column(Integer, ForeignKey("catalogo_servicos.id"), nullable=False)
     valor_recorrente = Column(DECIMAL(10, 2), nullable=False)
-    ciclo_cobranca = Column(ENUM('MENSAL', 'TRIMESTRAL', 'ANUAL'), nullable=False)
-    status = Column(ENUM('ATIVA', 'CANCELADA', 'INADIMPLENTE', 'TESTE'), nullable=False)
+    # CORREÇÃO AQUI
+    ciclo_cobranca = Column(Enum('MENSAL', 'TRIMESTRAL', 'ANUAL'), nullable=False)
+    # CORREÇÃO AQUI
+    status = Column(Enum('ATIVA', 'CANCELADA', 'INADIMPLENTE', 'TESTE'), nullable=False)
     data_inicio = Column(DATE, nullable=False)
     data_proxima_cobranca = Column(DATE, nullable=False)
