1249ab3ca221cf5e95a62518272bf4ee60bdb9c0
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 17:57:57 -0300

    Create fatura.py

---
+++ app/models/fatura.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ENUM, DATE, DECIMAL, ForeignKey
+from sqlalchemy.orm import relationship
+from ..core.db import Base
+class Fatura(Base):
+    __tablename__ = "faturas"
+    id = Column(Integer, primary_key=True, index=True)
+    assinatura_id = Column(Integer, ForeignKey("assinaturas.id"))
+    cliente_id = Column(Integer, ForeignKey("clientes.id"), nullable=False)
+    valor_total = Column(DECIMAL(10, 2), nullable=False)
+    data_emissao = Column(DATE, nullable=False)
+    data_vencimento = Column(DATE, nullable=False)
+    data_pagamento = Column(DATE)
+    status = Column(ENUM('PENDENTE', 'PAGA', 'VENCIDA', 'CANCELADA'), nullable=False)
+    itens = relationship("ItemFatura", back_populates="fatura")
+class ItemFatura(Base):
+    __tablename__ = "itens_fatura"
+    id = Column(Integer, primary_key=True, index=True)
+    fatura_id = Column(Integer, ForeignKey("faturas.id"), nullable=False)
+    descricao = Column(String(255), nullable=False)
+    quantidade = Column(DECIMAL(10, 2), nullable=False)
+    valor_unitario = Column(DECIMAL(10, 2), nullable=False)
+    valor_total_item = Column(DECIMAL(10, 2), nullable=False)
+    fatura = relationship("Fatura", back_populates="itens")
