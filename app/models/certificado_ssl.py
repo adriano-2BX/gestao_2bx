5a6dd880c89cafac54cae246ae998289c60ad7d1
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 18:18:46 -0300

    Update certificado_ssl.py

--- app/models/certificado_ssl.py
+++ app/models/certificado_ssl.py
@@ -1,5 +1,5 @@
 # app/models/certificado_ssl.py
-from sqlalchemy import Column, Integer, String, DATE, ENUM, DECIMAL
+from sqlalchemy import Column, Integer, String, DATE, Enum, DECIMAL
 from ..core.db import Base
 
 class CertificadoSSL(Base):
@@ -8,6 +8,7 @@ class CertificadoSSL(Base):
     id = Column(Integer, primary_key=True, index=True)
     nome_amigavel = Column(String(255), nullable=False)
     provedor_certificado = Column(String(100))
+    # CORREÇÃO AQUI
     tipo_certificado = Column(Enum('DOMINIO_UNICO', 'WILDCARD', 'MULTIDOMINIO'), nullable=False)
     dominio_principal = Column(String(255), index=True)
     data_emissao = Column(DATE)
