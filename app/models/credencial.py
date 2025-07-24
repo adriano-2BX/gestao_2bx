642ada09578bc71e14f2d5b69d39e9e59c2c423e
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 18:19:33 -0300

    Update credencial.py

--- app/models/credencial.py
+++ app/models/credencial.py
@@ -1,5 +1,5 @@
 # app/models/credencial.py
-from sqlalchemy import Column, Integer, String, ENUM, TEXT, TIMESTAMP
+from sqlalchemy import Column, Integer, String, Enum, TEXT, TIMESTAMP
 from sqlalchemy.sql import func
 from ..core.db import Base
 
@@ -7,7 +7,8 @@ class Credencial(Base):
     __tablename__ = "credenciais"
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String(255), nullable=False)
-    tipo_credencial = Column(ENUM('API_KEY', 'USUARIO_SENHA', 'TOKEN_JWT', 'CHAVE_SSH_PRIVADA', 'CERTIFICADO', 'OUTRO'), nullable=False)
+    # CORREÇÃO AQUI
+    tipo_credencial = Column(Enum('API_KEY', 'USUARIO_SENHA', 'TOKEN_JWT', 'CHAVE_SSH_PRIVADA', 'CERTIFICADO', 'OUTRO'), nullable=False)
     dados_credencial_encrypted = Column(TEXT, nullable=False)
     notas_seguras_encrypted = Column(TEXT)
     data_criacao = Column(TIMESTAMP, server_default=func.now())
