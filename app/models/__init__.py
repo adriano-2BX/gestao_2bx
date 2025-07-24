82b05253e4637dc54ca9641536d460e36ee0b99b
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 15:32:44 -0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .cliente import Cliente
 from .projeto import Projeto
-from .tarefa import Tarefa # Adicione esta linha
+from .tarefa import Tarefa
+from .usuario import Usuario # Adicione esta linha
