9c6f82b2cbc614d1c3b4a9c49457e9132da4dc12
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 15:37:37 -0300

    Create login.py

---
+++ app/api/endpoints/login.py
@@ -0,0 +1,31 @@
+# app/api/endpoints/login.py
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from datetime import timedelta
+
+from app import models, schemas
+from app.core.db import get_db
+from app.core import security
+from app.core.config import settings
+
+router = APIRouter()
+
+@router.post("/login/access-token", response_model=schemas.Token)
+def login_for_access_token(
+    db: Session = Depends(get_db),
+    form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user = db.query(models.Usuario).filter(models.Usuario.email == form_data.username).first()
+    if not user or not security.verify_password(form_data.password, user.senha_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="E-mail ou senha incorretos",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = security.create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
