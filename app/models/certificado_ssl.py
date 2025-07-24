dc054b7980ec4979e549ef8e03e7aa984878da40
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 15:55:03 -0300

    Create certificado_ssl.py

---
+++ app/models/certificado_ssl.py
@@ -0,0 +1,15 @@
+# app/models/certificado_ssl.py
+from sqlalchemy import Column, Integer, String, DATE, ENUM, DECIMAL
+from ..core.db import Base
+
+class CertificadoSSL(Base):
+    __tablename__ = "certificados_ssl"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome_amigavel = Column(String(255), nullable=False)
+    provedor_certificado = Column(String(100))
+    tipo_certificado = Column(Enum('DOMINIO_UNICO', 'WILDCARD', 'MULTIDOMINIO'), nullable=False)
+    dominio_principal = Column(String(255), index=True)
+    data_emissao = Column(DATE)
+    data_expiracao = Column(DATE, nullable=False)
+    custo_renovacao = Column(DECIMAL(10, 2))
