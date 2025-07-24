9b471e658aaf295a223f5a9b3fb1167ccfd20713
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 15:52:30 -0300

    Create dominio.py

---
+++ app/models/dominio.py
@@ -0,0 +1,15 @@
+# app/models/dominio.py
+from sqlalchemy import Column, Integer, String, DATE, ENUM, DECIMAL, ForeignKey
+from ..core.db import Base
+
+class Dominio(Base):
+    __tablename__ = "dominios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome_completo_fqdn = Column(String(255), unique=True, nullable=False, index=True)
+    parent_id = Column(Integer, ForeignKey("dominios.id"))
+    provedor_registro = Column(String(100))
+    data_registro = Column(DATE)
+    data_expiracao = Column(DATE, nullable=False)
+    status_dominio = Column(Enum('ATIVO', 'EXPIRADO', 'EM_REDENCAO', 'PENDENTE'), nullable=False, default='ATIVO')
+    custo_anual_renovacao = Column(DECIMAL(10, 2))
