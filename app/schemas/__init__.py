a05ffd299a579f589d553a7ad5ac85407deb6d50
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 17:41:39 -0300

    Update __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
+# app/schemas/__init__.py
 from .cliente import Cliente, ClienteCreate, ClienteUpdate, ClienteBase
 from .projeto import Projeto, ProjetoCreate, ProjetoUpdate, ProjetoBase
 from .tarefa import Tarefa, TarefaCreate, TarefaUpdate, TarefaBase
@@ -6,8 +7,9 @@ from .token import Token, TokenData
 from .servidor import Servidor, ServidorCreate, ServidorUpdate, ServidorBase
 from .aplicacao import Aplicacao, AplicacaoCreate, AplicacaoUpdate, AplicacaoBase
 from .dominio import Dominio, DominioCreate, DominioUpdate, DominioBase
-from .certificado_ssl import CertificadoSSL, CertificadoSSLCreate, CertificadoSSLUpdate, CertificadoSSLBase # Adicione esta linha
-from .conexao import Conexao, ConexaoCreate, ConexaoBase # Adicione esta linha
-from .permissao import Permissao, PermissaoCreate, PermissaoBase # Adicione esta linha
-from .papel import Papel, PapelCreate, PapelUpdate, PapelBase, LinkPermissaoToPapel # Adicione esta linha
-from .time import Time, TimeCreate, TimeUpdate, TimeBase, LinkUsuarioToTime # Adicione esta linha
+from .certificado_ssl import CertificadoSSL, CertificadoSSLCreate, CertificadoSSLUpdate, CertificadoSSLBase
+from .conexao import Conexao, ConexaoCreate, ConexaoBase
+from .permissao import Permissao, PermissaoCreate, PermissaoBase
+from .papel import Papel, PapelCreate, PapelUpdate, PapelBase, LinkPermissaoToPapel
+from .time import Time, TimeCreate, TimeUpdate, TimeBase, LinkUsuarioToTime
+from .credencial import Credencial, CredencialCreate, CredencialUpdate, CredencialRevelada
