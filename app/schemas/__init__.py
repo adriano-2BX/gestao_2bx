30ce5ed65447708e75c72907775b1448129d1872
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 15:48:12 -0300

    Update __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
-# app/schemas/__init__.py
 from .cliente import Cliente, ClienteCreate, ClienteUpdate, ClienteBase
 from .projeto import Projeto, ProjetoCreate, ProjetoUpdate, ProjetoBase
 from .tarefa import Tarefa, TarefaCreate, TarefaUpdate, TarefaBase
 from .usuario import Usuario, UsuarioCreate, UsuarioBase
-from .token import Token, TokenData # Adicione esta linha
+from .token import Token, TokenData
+from .servidor import Servidor, ServidorCreate, ServidorUpdate, ServidorBase # Adicione esta linha
