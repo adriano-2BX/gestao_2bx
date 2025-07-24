dbc7ffa3800097d461279ecfeac374420ad9eb22
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 15:32:58 -0300

    Update __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .cliente import Cliente, ClienteCreate, ClienteUpdate, ClienteBase
 from .projeto import Projeto, ProjetoCreate, ProjetoUpdate, ProjetoBase
-from .tarefa import Tarefa, TarefaCreate, TarefaUpdate, TarefaBase # Adicione esta linha
+from .tarefa import Tarefa, TarefaCreate, TarefaUpdate, TarefaBase
+from .usuario import Usuario, UsuarioCreate, UsuarioBase # Adicione esta linha
