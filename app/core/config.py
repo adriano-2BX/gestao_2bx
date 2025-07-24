42eef97e42f6f3021094ba794167a22d50958a50
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-24 17:38:55 -0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+# app/core/config.py
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
@@ -5,6 +6,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ENCRYPTION_KEY: str
 
     class Config:
         env_file = ".env"
